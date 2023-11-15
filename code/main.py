53756298b573615bb9e565dc9eb530a6b52417dc
Damian Wiśniewski <dev@thehardway.pl>
2023-11-15 22:27:16 +0100

    Basic setup

--- code/main.py
+++ code/main.py
@@ -1,12 +1,15 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Sprout land")
         self.clock = pygame.time.Clock()
+        self.level = Level()
 
     def run(self):
         while True:
@@ -16,6 +19,7 @@ class Game:
                     sys.exit()
 
             dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pygame.display.update()
 
 
