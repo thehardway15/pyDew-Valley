e04af75467e71c4d7ca22bdb8fd2afd835178e35
Damian Wiśniewski <dev@thehardway.pl>
2023-11-20 23:43:17 +0100

    Particle

--- code/sprites.py
+++ code/sprites.py
@@ -41,6 +41,24 @@ class WildFlower(Generic):
         self.hitbox = self.rect.copy().inflate((-20, -self.rect.height * 0.9))
 
 
+class Particle(Generic):
+    def __init__(self, pos, surf, groups, z, duration = 200):
+        super().__init__(pos=pos, surf=surf, groups=groups, z=z)
+        self.start_time = pygame.time.get_ticks()
+        self.duration = duration
+
+        # white surface
+        mask_surf = pygame.mask.from_surface(self.image)
+        new_surf = mask_surf.to_surface()
+        new_surf.set_colorkey((0, 0, 0))
+        self.image = new_surf
+
+    def update(self, dt):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time > self.duration:
+            self.kill()
+
+
 class Tree(Generic):
 
     def __init__(self, pos, surf, groups, name):
@@ -75,10 +93,17 @@ class Tree(Generic):
 
         if len(self.apple_sprites.sprites()) > 0:
             random_apple = choice(self.apple_sprites.sprites())
+            Particle(
+                pos = random_apple.rect.topleft,
+                surf = random_apple.image,
+                groups = random_apple.groups(),
+                z = LAYERS['fruit']
+            )
             random_apple.kill()
 
     def check_death(self):
         if self.health <= 0:
+            Particle(self.rect.topleft, self.image, self.groups(), LAYERS['fruit'], 300)
             self.image = self.stump_surf
             self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
             self.hitbox = self.rect.copy().inflate((-10, -self.rect.height * 0.6))
